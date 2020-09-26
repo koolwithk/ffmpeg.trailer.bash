4ce4600f99cc29c3f110a0b544ed8dceec4c80d2
koolwithk <koolwithk>
2020-09-26 02:06:13 -0700

    added comment

--- 4.fade.sh
+++ 4.fade.sh
@@ -1,9 +1,8 @@
 function fade() {
   cd $input_path
-
-  subclip_count=$(ls -ltr | awk '{print $NF}' | grep 'subclip.mp4' | wc -l)
-
+  
   #fade in
+  #get 1st subclip
   fade_in_video=$(ls -ltr | awk '{print $NF}' | grep 'subclip' | sed -n 1p)
   fade_in_video="1.subclip.mp4"
   echo "$fade_in_video"
@@ -11,8 +10,9 @@ function fade() {
   ffmpeg -y -i $fade_in_video -vf fade=in:st=0:d=2 fade_in.$fade_in_video
   mv -v fade_in.$fade_in_video $fade_in_video
   
-
   #fade out
+  #get the last subclip
+  subclip_count=$(ls -ltr | awk '{print $NF}' | grep 'subclip.mp4' | wc -l)
   fade_out_video=$(ls -ltr | awk '{print $NF}' | grep 'subclip' | sed 'x;$!d')
   fade_out_video="$subclip_count.subclip.mp4"
   echo "$fade_out_video"
